9da837264ada2add1fde37cd835ffae88f94a351
VINICIUS SANTANA DE LIMA PAZ <vinicius.slpaz@senacsp.edu.br>
2023-06-27 17:14:29 -0300

    Criamos os controllers de cargo e criação de views

---
+++ app/Http/Controllers/CargoController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\{
+    Cargo, User
+};
+use Ramsey\Uuid\Type\Integer;
+
+class CargoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $cargos = Cargo::orderBy('cargo');
+        return view('cargo.index')->with(compact('cargo'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $cargo = null;
+        return view('cargo.form')->with(compact('cargo'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $cargo = Cargo::created($request->all());
+        return redirect()->route('cargo.index')->with('sucess', 'Cadastrado com Sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(integer $id)
+    {
+        $cargo = Cargo::find($id);
+        return view('cargo.show')->with(compact('cargo'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(int $id)
+    {
+        $cargo = Cargo::find($id);
+        return view('cargo.form')->with(compact('cargo'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,int $id)
+    {
+        $cargo = Cargo::find($id);
+        $cargo->update($request->all());
+        return redirect()->route('cargo.index')->with('sucess','Atualizado com Sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(int $id)
+    {
+        Cargo::find($id)->delete();
+        return redirect()->back()->with('destroy', 'Excluído com Sucesso!');
+    }
+}
